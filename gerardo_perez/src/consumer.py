457e5ed671856ab3ccfd005aad7c3b721ed4dd74
Gerardo Pérez <geperez@apexsystems.com>
2024-11-29 13:21:49 -0600

    Guardado de datos en SQLite

--- gerardo_perez/src/consumer.py
+++ gerardo_perez/src/consumer.py
@@ -1,5 +1,8 @@
 from kafka import KafkaConsumer
 import sys
+import json
+from datetime import datetime
+import sqlite3
 
 TOPIC = 'bitusdt-avg-topic'
 
@@ -12,9 +15,25 @@ consumer = KafkaConsumer(
     auto_offset_reset='earliest'
 )
 
+conn = sqlite3.connect(":memory:")
+cur = conn.cursor()
+cur.execute('CREATE TABLE bitusdt(id INTEGER PRIMARY KEY, closeTime TEXT, price REAL, mins TEXT)')
+
 try:
     for message in consumer:
-        print(message.value)
+        message_topic = message.topic
+        message_value = message.value
+        json_value = json.loads(message_value)
+        json_value['closeTime'] = datetime.fromtimestamp(json_value['closeTime']/1000).strftime('%Y-%m-%d %H:%M:%S')
+        print(json_value)
+        
+        cur.execute('INSERT INTO bitusdt(closeTime, price, mins) VALUES (:closeTime, :price, :mins)', json_value)
+        conn.commit()
+
 except KeyboardInterrupt:
     print('Cerrando consumer...')
+    cur.close()
+    conn.close()
     sys.exit()
+
+
